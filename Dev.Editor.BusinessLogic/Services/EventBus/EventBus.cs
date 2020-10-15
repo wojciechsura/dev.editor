7483f2c3d3618aa6836e94e37eb8d2c9cf114ca9
Wojciech Sura <wsura@wscad.com>
2020-10-15 19:01:37 +0200

    Fixed event bus

--- Dev.Editor.BusinessLogic/Services/EventBus/EventBus.cs
+++ Dev.Editor.BusinessLogic/Services/EventBus/EventBus.cs
@@ -1,5 +1,6 @@
 ﻿using Dev.Editor.BusinessLogic.Models.Events;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,19 +8,41 @@ using System.Threading.Tasks;
 
 namespace Dev.Editor.BusinessLogic.Services.EventBus
 {
-    class EventBus : IEventBus
+    internal class EventBus : IEventBus
     {
-        private class ListenerInfo<T>
-            where T : BaseEvent
+        private abstract class BaseListenerInfo
         {
-            public ListenerInfo(IEventListener<T> listener, bool notifySelf)
+            public BaseListenerInfo(bool notifySelf)
             {
-                Listener = listener;
                 NotifySelf = notifySelf;
             }
 
-            public IEventListener<T> Listener { get; }
+            public abstract bool ContainsListener(object listener);
+            public abstract void Receive(object @event);
+
             public bool NotifySelf { get; }
+        }
+
+        private class ListenerInfo<T> : BaseListenerInfo
+                where T : BaseEvent
+        {
+            private readonly IEventListener<T> listener;
+
+            public ListenerInfo(IEventListener<T> listener, bool notifySelf)
+              : base(notifySelf)
+            {
+                this.listener = listener;
+            }
+
+            public override bool ContainsListener(object listener)
+            {
+                return this.listener == listener;
+            }
+
+            public override void Receive(object @event)
+            {
+                listener.Receive((T)@event);
+            }
         }
 
         private readonly Dictionary<Type, object> listeners = new Dictionary<Type, object>();
@@ -31,20 +54,20 @@ namespace Dev.Editor.BusinessLogic.Services.EventBus
 
             Type t = typeof(T);
 
-            List<ListenerInfo<T>> eventListeners;
+            List<BaseListenerInfo> eventListeners;
             if (!listeners.ContainsKey(t))
             {
-                eventListeners = new List<ListenerInfo<T>>();
+                eventListeners = new List<BaseListenerInfo>();
                 listeners[t] = eventListeners;
             }
             else
             {
-                eventListeners = listeners[t] as List<ListenerInfo<T>>;
+                eventListeners = listeners[t] as List<BaseListenerInfo>;
                 if (eventListeners == null)
                     throw new InvalidOperationException("Invalid list type in dictionary!");
             }
 
-            if (!eventListeners.Any(li => li.Listener == listener))
+            if (!eventListeners.Any(li => li.ContainsListener(listener)))
             {
                 var listenerInfo = new ListenerInfo<T>(listener, notifySelf);
                 eventListeners.Add(listenerInfo);
@@ -61,9 +84,9 @@ namespace Dev.Editor.BusinessLogic.Services.EventBus
             if (!listeners.ContainsKey(t))
                 return;
 
-            List<ListenerInfo<T>> eventListeners = listeners[t] as List<ListenerInfo<T>>;
-            
-            var info = eventListeners.FirstOrDefault(el => el.Listener == listener);
+            List<BaseListenerInfo> eventListeners = listeners[t] as List<BaseListenerInfo>;
+
+            var info = eventListeners.FirstOrDefault(el => el.ContainsListener(listener));
             if (info != null)
                 eventListeners.Remove(info);
 
@@ -71,20 +94,48 @@ namespace Dev.Editor.BusinessLogic.Services.EventBus
                 listeners.Remove(t);
         }
 
+        public void UnregisterAll(object listener)
+        {
+            if (listener == null)
+                throw new ArgumentNullException(nameof(listener));
+
+            foreach (var pair in listeners)
+            {
+                var list = pair.Value as List<BaseListenerInfo>;
+
+                int i = 0;
+                while (i < list.Count)
+                {
+                    if ((list[i]).ContainsListener(listener))
+                    {
+                        list.RemoveAt(i);
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+        }
+
         public void Send<T>(object sender, T @event) where T : BaseEvent
         {
             Type t = typeof(T);
-            if (listeners.ContainsKey(t))
-            {
-                List<ListenerInfo<T>> eventListeners = listeners[t] as List<ListenerInfo<T>>;
-                
-                foreach (var info in eventListeners)
-                {
-                    if (!info.NotifySelf && info.Listener == sender)
-                        continue;
 
-                    info.Listener.Receive(@event);
-                }                    
+            while (t != typeof(object))
+            {
+                if (listeners.ContainsKey(t))
+                {
+                    List<BaseListenerInfo> eventListeners = listeners[t] as List<BaseListenerInfo>;
+
+                    foreach (var info in eventListeners)
+                    {
+                        if (info.NotifySelf || !info.ContainsListener(sender))
+                            info.Receive(@event);
+                    }
+                }
+
+                t = t.BaseType;
             }
         }
     }
