9570f2518c5b2390d526aa024a4b002fdd8f6b38
Wojciech Sura <wsura@wscad.com>
2021-01-19 10:13:55 +0100

    Changed icon for finding duplicated items

--- Dev.Editor.BusinessLogic/ViewModels/Main/MainWindowViewModel.cs
+++ Dev.Editor.BusinessLogic/ViewModels/Main/MainWindowViewModel.cs
@@ -1091,7 +1091,7 @@ namespace Dev.Editor.BusinessLogic.ViewModels.Main
             SortLinesDescendingCommand = commandRepositoryService.RegisterCommand(Resources.Strings.Ribbon_Lines_Ordering_SortDescending, "SortDescending16.png", obj => DoSortDescending(), documentIsTextCondition);
             RemoveEmptyLinesCommand = commandRepositoryService.RegisterCommand(Resources.Strings.Ribbon_Lines_Cleanup_RemoveEmptyLines, "DeleteLine16.png", obj => DoRemoveEmptyLines(), documentIsTextCondition);
             RemoveWhitespaceLinesCommand = commandRepositoryService.RegisterCommand(Resources.Strings.Ribbon_Lines_Cleanup_RemoveWhitespaceLines, "DeleteLine16.png", obj => DoRemoveWhitespaceLines(), documentIsTextCondition);
-            FindDuplicatedLinesCommand = commandRepositoryService.RegisterCommand(Resources.Strings.Ribbon_Lines_Tools_FindDuplicatedLines, "Compare16.png", obj => DoFindDuplicatedLines());
+            FindDuplicatedLinesCommand = commandRepositoryService.RegisterCommand(Resources.Strings.Ribbon_Lines_Tools_FindDuplicatedLines, "Duplicates16.png", obj => DoFindDuplicatedLines());
 
             LowercaseCommand = commandRepositoryService.RegisterCommand(Resources.Strings.Ribbon_Text_Transform_Case_Lowercase, "Case16.png", obj => DoLowercase(), documentIsTextCondition);
             UppercaseCommand = commandRepositoryService.RegisterCommand(Resources.Strings.Ribbon_Text_Transform_Case_Uppercase, "Case16.png", obj => DoUppercase(), documentIsTextCondition);
